dade1b3535ab6f6001f05d9dac0578afa9e844f6
harshhx <hg070401@gmail.com>
2021-03-17 20:28:40 +0530

    17th march 2021

---
+++ Two distinct points.py
@@ -0,0 +1,12 @@
+q = int(input())
+for _ in range(q):
+	l1,r1,l2,r2 = map(int,input().split())
+
+	if l1 != l2:
+	    print(l1,l2)
+	elif l1 != r2:
+	    print(l1,r2)
+	elif r1 != l2:
+	    print(r1,l2)
+	elif r1 != r2:
+	    print(r1,r2)
